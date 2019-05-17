84a5043610a05354aeed7fabb93814ae5dc34802
bill.tang <bill.tang@sap.com>
2019-05-17 17:50:37 +0800

    use callback

--- jsonwebtoken/app.js
+++ jsonwebtoken/app.js
@@ -1,5 +1,8 @@
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
-var privateKey = fs.readFileSync('../cmder/rsa_private_unencrypted.pem');
-var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256'});
-console.log(token);
+var privateKey = fs.readFileSync('./rsa_private_unencrypted.pem');
+jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256'}, (err, token) => {
+	console.log(err);
+	console.log("======");
+	console.log(token);
+});
